7f8aabbd9074d78185345a09cb087fa670097aa8
EC2 Default User <ec2-user@ip-172-31-18-51.ec2.internal>
2023-01-11 06:45:35 +0000

    [Fix]Admin top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords,], controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "customer/registrations",
   sessions: 'customer/sessions'
   }
 
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
-
   root to: "homes#top"
   get "mypage", to: 'customers#show'
+  get "mypage/edit", to: 'customers#edit'
+  patch "mypage", to: 'customers#update'
+  get "unsubscribe", to: 'customers#unsubscribe'
+  patch "withdraw", to: 'customers#withdraw'
   get "home/about"=> "homes#about", as: "about"
 
+
   scope module: :public do
+    resources :items, only: [:index,:show]
+    delete "cart_items/destroy_all", to: 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
   end
   namespace :admin do
-    get "homes/top", as: "top"
+    root to: "homes#top"
+    resources :customers, only: [:index]
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
