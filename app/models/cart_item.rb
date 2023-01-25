0f85cea10da699e749a499c717e156219fc0d7a4
EC2 Default User <ec2-user@ip-172-31-18-51.ec2.internal>
2023-01-25 08:00:35 +0000

    [Fix]cart_item.rb

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,6 +3,6 @@ class CartItem < ApplicationRecord
   belongs_to :item, foreign_key: :item_id
 
   def total_price
-    (self.item.price * amount * 1.1).round
+    (self.item.add_tax_price).round
   end
 end
